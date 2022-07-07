3b9c3efeb9644c4a3864936625b0d9db6c646d8e
trotrosalaYT <86182280+trotrosalaYT@users.noreply.github.com>
2022-07-07 21:01:47 +0200

    Update Mining Simulator 2.lua

--- Mining Simulator 2.lua
+++ Mining Simulator 2.lua
@@ -33,18 +33,43 @@ local maintab = Instance.new("TextButton")
 local UICorner_13 = Instance.new("UICorner")
 local mainframe = Instance.new("Frame")
 local UICorner_14 = Instance.new("UICorner")
-local drill = Instance.new("TextButton")
+local ownerlabel = Instance.new("TextLabel")
 local UICorner_15 = Instance.new("UICorner")
-local infotab = Instance.new("TextButton")
+local discord = Instance.new("TextLabel")
 local UICorner_16 = Instance.new("UICorner")
-local infoframe = Instance.new("Frame")
+local tag = Instance.new("TextLabel")
 local UICorner_17 = Instance.new("UICorner")
+local label_2 = Instance.new("TextLabel")
+local UICorner_18 = Instance.new("UICorner")
+local infotab = Instance.new("TextButton")
+local UICorner_19 = Instance.new("UICorner")
+local infoframe = Instance.new("Frame")
+local UICorner_20 = Instance.new("UICorner")
 local infolabel = Instance.new("TextLabel")
 local autoegg = Instance.new("TextButton")
-local UICorner_18 = Instance.new("UICorner")
-local eggframe = Instance.new("Frame")
-local UICorner_19 = Instance.new("UICorner")
-local egglabel = Instance.new("TextLabel")
+local UICorner_21 = Instance.new("UICorner")
+local eggframe = Instance.new("ScrollingFrame")
+local UICorner_22 = Instance.new("UICorner")
+local basic = Instance.new("TextButton")
+local UICorner_23 = Instance.new("UICorner")
+local spotted = Instance.new("TextButton")
+local UICorner_24 = Instance.new("UICorner")
+local forest = Instance.new("TextButton")
+local UICorner_25 = Instance.new("UICorner")
+local exotic = Instance.new("TextButton")
+local UICorner_26 = Instance.new("UICorner")
+local arctic = Instance.new("TextButton")
+local UICorner_27 = Instance.new("UICorner")
+local ice = Instance.new("TextButton")
+local UICorner_28 = Instance.new("UICorner")
+local dark = Instance.new("TextButton")
+local UICorner_29 = Instance.new("UICorner")
+local volcanic = Instance.new("TextButton")
+local UICorner_30 = Instance.new("UICorner")
+local underworld = Instance.new("TextButton")
+local UICorner_31 = Instance.new("UICorner")
+local crystal = Instance.new("TextButton")
+local UICorner_32 = Instance.new("UICorner")
 
 --Properties:
 
@@ -55,7 +80,7 @@ miningsim.ZIndexBehavior = Enum.ZIndexBehavior.Sibling
 gui.Name = "gui"
 gui.Parent = miningsim
 gui.BackgroundColor3 = Color3.fromRGB(48, 0, 203)
-gui.Position = UDim2.new(0.285983682, 0, 0.110159121, 0)
+gui.Position = UDim2.new(0.270455748, 0, 0.116279073, 0)
 gui.Size = UDim2.new(0, 724, 0, 323)
 gui.Visible = true
 gui.Active = true
@@ -75,7 +100,7 @@ destroy.TextScaled = true
 destroy.TextSize = 14.000
 destroy.TextWrapped = true
 destroy.MouseButton1Down:Connect(function()
-	game.CoreGui.miningsim:Destroy()
+	miningsim:Destroy()
 end)
 
 UICorner_2.Parent = destroy
@@ -109,6 +134,7 @@ tptab.MouseButton1Down:Connect(function()
 	mainframe.Visible = false
 	eggframe.Visible = false
 end)
+
 
 UICorner_4.Parent = tptab
 
@@ -289,6 +315,7 @@ maintab.MouseButton1Down:Connect(function()
 	eggframe.Visible = false
 end)
 
+
 UICorner_13.Parent = maintab
 
 mainframe.Name = "mainframe"
@@ -296,9 +323,63 @@ mainframe.Parent = gui
 mainframe.BackgroundColor3 = Color3.fromRGB(48, 0, 203)
 mainframe.Position = UDim2.new(0.276243091, 0, 0.154798761, 0)
 mainframe.Size = UDim2.new(0, 524, 0, 223)
-mainframe.Visible = true
 
 UICorner_14.Parent = mainframe
+
+ownerlabel.Name = "ownerlabel"
+ownerlabel.Parent = mainframe
+ownerlabel.BackgroundColor3 = Color3.fromRGB(0, 4, 226)
+ownerlabel.Size = UDim2.new(0, 524, 0, 50)
+ownerlabel.Font = Enum.Font.PatrickHand
+ownerlabel.Text = "Hello its me! The Owner of Neku Hub!"
+ownerlabel.TextColor3 = Color3.fromRGB(0, 0, 0)
+ownerlabel.TextScaled = true
+ownerlabel.TextSize = 14.000
+ownerlabel.TextWrapped = true
+
+UICorner_15.Parent = ownerlabel
+
+discord.Name = "discord"
+discord.Parent = mainframe
+discord.BackgroundColor3 = Color3.fromRGB(1, 189, 226)
+discord.Position = UDim2.new(0, 0, 0.224215239, 0)
+discord.Size = UDim2.new(0, 524, 0, 50)
+discord.Font = Enum.Font.PatrickHand
+discord.Text = "If you want to join the discord, you can here: https://discord.gg/PTD7gGGR"
+discord.TextColor3 = Color3.fromRGB(0, 0, 0)
+discord.TextScaled = true
+discord.TextSize = 14.000
+discord.TextWrapped = true
+
+UICorner_16.Parent = discord
+
+tag.Name = "tag"
+tag.Parent = mainframe
+tag.BackgroundColor3 = Color3.fromRGB(114, 8, 189)
+tag.Position = UDim2.new(-0.00190839695, 0, 0.448430479, 0)
+tag.Size = UDim2.new(0, 524, 0, 50)
+tag.Font = Enum.Font.PatrickHand
+tag.Text = "My Tag is: trot#0908"
+tag.TextColor3 = Color3.fromRGB(0, 0, 0)
+tag.TextScaled = true
+tag.TextSize = 14.000
+tag.TextWrapped = true
+
+UICorner_17.Parent = tag
+
+label_2.Name = "label"
+label_2.Parent = mainframe
+label_2.BackgroundColor3 = Color3.fromRGB(0, 203, 30)
+label_2.Position = UDim2.new(0, 0, 0.672645688, 0)
+label_2.Size = UDim2.new(0, 524, 0, 50)
+label_2.Font = Enum.Font.PatrickHand
+label_2.Text = "Have fun using!^^"
+label_2.TextColor3 = Color3.fromRGB(0, 0, 0)
+label_2.TextScaled = true
+label_2.TextSize = 14.000
+label_2.TextWrapped = true
+
+UICorner_18.Parent = label_2
 
 infotab.Name = "infotab"
 infotab.Parent = gui
@@ -318,7 +399,7 @@ infotab.MouseButton1Down:Connect(function()
 	eggframe.Visible = false
 end)
 
-UICorner_16.Parent = infotab
+UICorner_19.Parent = infotab
 
 infoframe.Name = "infoframe"
 infoframe.Parent = gui
@@ -327,7 +408,7 @@ infoframe.Position = UDim2.new(0.274861872, 0, 0.154798761, 0)
 infoframe.Size = UDim2.new(0, 525, 0, 223)
 infoframe.Visible = false
 
-UICorner_17.Parent = infoframe
+UICorner_20.Parent = infoframe
 
 infolabel.Name = "infolabel"
 infolabel.Parent = infoframe
@@ -348,7 +429,7 @@ autoegg.BackgroundColor3 = Color3.fromRGB(96, 23, 206)
 autoegg.Position = UDim2.new(0, 0, 0.619195044, 0)
 autoegg.Size = UDim2.new(0, 200, 0, 50)
 autoegg.Font = Enum.Font.PatrickHand
-autoegg.Text = "Auto Eggs"
+autoegg.Text = "Egg open (one time)"
 autoegg.TextColor3 = Color3.fromRGB(0, 0, 0)
 autoegg.TextScaled = true
 autoegg.TextSize = 14.000
@@ -360,31 +441,210 @@ autoegg.MouseButton1Down:Connect(function()
 	eggframe.Visible = true
 end)
 
-UICorner_18.Parent = autoegg
+UICorner_21.Parent = autoegg
 
 eggframe.Name = "eggframe"
 eggframe.Parent = gui
+eggframe.Active = true
 eggframe.BackgroundColor3 = Color3.fromRGB(48, 0, 203)
-eggframe.Position = UDim2.new(0.274861872, 0, 0.154798761, 0)
-eggframe.Size = UDim2.new(0, 525, 0, 223)
+eggframe.Position = UDim2.new(0.276243091, 0, 0.154798761, 0)
+eggframe.Size = UDim2.new(0, 524, 0, 223)
+eggframe.Visible = false
 
-UICorner_19.Parent = eggframe
+UICorner_22.Parent = eggframe
 
-egglabel.Name = "egglabel"
-egglabel.Parent = eggframe
-egglabel.BackgroundColor3 = Color3.fromRGB(255, 255, 255)
-egglabel.BackgroundTransparency = 1.000
-egglabel.Size = UDim2.new(0, 525, 0, 223)
-egglabel.Font = Enum.Font.PatrickHand
-egglabel.Text = "Coming soon"
-egglabel.TextColor3 = Color3.fromRGB(0, 0, 0)
-egglabel.TextScaled = true
-egglabel.TextSize = 14.000
-egglabel.TextWrapped = true
+basic.Name = "basic"
+basic.Parent = eggframe
+basic.BackgroundColor3 = Color3.fromRGB(214, 137, 5)
+basic.Size = UDim2.new(0, 200, 0, 50)
+basic.Font = Enum.Font.PatrickHand
+basic.Text = "Basic Egg"
+basic.TextColor3 = Color3.fromRGB(0, 0, 0)
+basic.TextScaled = true
+basic.TextSize = 14.000
+basic.TextWrapped = true
+basic.MouseButton1Down:Connect(function()
+	local A_1 = "Basic Egg"
+	local Event = game:GetService("ReplicatedStorage").Events.OpenEgg
+	Event:FireServer(A_1)
+end)
+
+UICorner_23.Parent = basic
+
+spotted.Name = "spotted"
+spotted.Parent = eggframe
+spotted.BackgroundColor3 = Color3.fromRGB(4, 199, 202)
+spotted.Position = UDim2.new(0.5, 0, -0.00154798757, 0)
+spotted.Size = UDim2.new(0, 200, 0, 50)
+spotted.Font = Enum.Font.PatrickHand
+spotted.Text = "Spotted Egg"
+spotted.TextColor3 = Color3.fromRGB(0, 0, 0)
+spotted.TextScaled = true
+spotted.TextSize = 14.000
+spotted.TextWrapped = true
+spotted.MouseButton1Down:Connect(function()
+	local A_1 = "Spotted Egg"
+	local Event = game:GetService("ReplicatedStorage").Events.OpenEgg
+	Event:FireServer(A_1)
+end)
+
+UICorner_24.Parent = spotted
+
+forest.Name = "forest"
+forest.Parent = eggframe
+forest.BackgroundColor3 = Color3.fromRGB(20, 189, 2)
+forest.Position = UDim2.new(-0.00190842152, 0, 0.133126929, 0)
+forest.Size = UDim2.new(0, 200, 0, 50)
+forest.Font = Enum.Font.PatrickHand
+forest.Text = "Forest Egg"
+forest.TextColor3 = Color3.fromRGB(0, 0, 0)
+forest.TextScaled = true
+forest.TextSize = 14.000
+forest.TextWrapped = true
+forest.MouseButton1Down:Connect(function()
+	local A_1 = "Forest Egg"
+	local Event = game:GetService("ReplicatedStorage").Events.OpenEgg
+	Event:FireServer(A_1)
+end)
+
+UICorner_25.Parent = forest
+
+exotic.Name = "exotic"
+exotic.Parent = eggframe
+exotic.BackgroundColor3 = Color3.fromRGB(188, 173, 2)
+exotic.Position = UDim2.new(0.5, 0, 0.133126929, 0)
+exotic.Size = UDim2.new(0, 200, 0, 50)
+exotic.Font = Enum.Font.PatrickHand
+exotic.Text = "Exotic Egg"
+exotic.TextColor3 = Color3.fromRGB(0, 0, 0)
+exotic.TextScaled = true
+exotic.TextSize = 14.000
+exotic.TextWrapped = true
+exotic.MouseButton1Down:Connect(function()
+	local A_1 = "Exotic Egg"
+	local Event = game:GetService("ReplicatedStorage").Events.OpenEgg
+	Event:FireServer(A_1)
+end)
+
+UICorner_26.Parent = exotic
+
+arctic.Name = "arctic"
+arctic.Parent = eggframe
+arctic.BackgroundColor3 = Color3.fromRGB(0, 3, 191)
+arctic.Position = UDim2.new(-0.00190842152, 0, 0.267801851, 0)
+arctic.Size = UDim2.new(0, 200, 0, 50)
+arctic.Font = Enum.Font.PatrickHand
+arctic.Text = "Arctic Egg"
+arctic.TextColor3 = Color3.fromRGB(0, 0, 0)
+arctic.TextScaled = true
+arctic.TextSize = 14.000
+arctic.TextWrapped = true
+arctic.MouseButton1Down:Connect(function()
+	local A_1 = "Arctic Egg"
+	local Event = game:GetService("ReplicatedStorage").Events.OpenEgg
+	Event:FireServer(A_1)
+end)
+
+UICorner_27.Parent = arctic
+
+ice.Name = "ice"
+ice.Parent = eggframe
+ice.BackgroundColor3 = Color3.fromRGB(0, 178, 198)
+ice.Position = UDim2.new(0.5, 0, 0.267801851, 0)
+ice.Size = UDim2.new(0, 200, 0, 50)
+ice.Font = Enum.Font.PatrickHand
+ice.Text = "Ice Egg"
+ice.TextColor3 = Color3.fromRGB(0, 0, 0)
+ice.TextScaled = true
+ice.TextSize = 14.000
+ice.TextWrapped = true
+ice.MouseButton1Down:Connect(function()
+	local A_1 = "Ice Egg"
+	local Event = game:GetService("ReplicatedStorage").Events.OpenEgg
+	Event:FireServer(A_1)
+end)
+
+UICorner_28.Parent = ice
+
+dark.Name = "dark"
+dark.Parent = eggframe
+dark.BackgroundColor3 = Color3.fromRGB(15, 0, 106)
+dark.Position = UDim2.new(-0.00190842152, 0, 0.386996895, 0)
+dark.Size = UDim2.new(0, 200, 0, 50)
+dark.Font = Enum.Font.PatrickHand
+dark.Text = "Dark Egg"
+dark.TextColor3 = Color3.fromRGB(0, 0, 0)
+dark.TextScaled = true
+dark.TextSize = 14.000
+dark.TextWrapped = true
+dark.MouseButton1Down:Connect(function()
+	local A_1 = "Dark Egg"
+	local Event = game:GetService("ReplicatedStorage").Events.OpenEgg
+	Event:FireServer(A_1)
+end)
+
+UICorner_29.Parent = dark
+
+volcanic.Name = "volcanic"
+volcanic.Parent = eggframe
+volcanic.BackgroundColor3 = Color3.fromRGB(195, 48, 0)
+volcanic.Position = UDim2.new(0.5, 0, 0.386996895, 0)
+volcanic.Size = UDim2.new(0, 200, 0, 50)
+volcanic.Font = Enum.Font.PatrickHand
+volcanic.Text = "Volcanic Egg"
+volcanic.TextColor3 = Color3.fromRGB(0, 0, 0)
+volcanic.TextScaled = true
+volcanic.TextSize = 14.000
+volcanic.TextWrapped = true
+volcanic.MouseButton1Down:Connect(function()
+	local A_1 = "Volcanic Egg"
+	local Event = game:GetService("ReplicatedStorage").Events.OpenEgg
+	Event:FireServer(A_1)
+end)
+
+UICorner_30.Parent = volcanic
+
+underworld.Name = "underworld"
+underworld.Parent = eggframe
+underworld.BackgroundColor3 = Color3.fromRGB(85, 8, 153)
+underworld.Position = UDim2.new(0, 0, 0.5, 0)
+underworld.Size = UDim2.new(0, 200, 0, 50)
+underworld.Font = Enum.Font.PatrickHand
+underworld.Text = "Underworld Egg"
+underworld.TextColor3 = Color3.fromRGB(0, 0, 0)
+underworld.TextScaled = true
+underworld.TextSize = 14.000
+underworld.TextWrapped = true
+underworld.MouseButton1Down:Connect(function()
+	local A_1 = "Underworld Egg"
+	local Event = game:GetService("ReplicatedStorage").Events.OpenEgg
+	Event:FireServer(A_1)
+end)
+
+UICorner_31.Parent = underworld
+
+crystal.Name = "crystal"
+crystal.Parent = eggframe
+crystal.BackgroundColor3 = Color3.fromRGB(119, 40, 125)
+crystal.Position = UDim2.new(0.5, 0, 0.49999997, 0)
+crystal.Size = UDim2.new(0, 200, 0, 50)
+crystal.Font = Enum.Font.PatrickHand
+crystal.Text = "Crystal Egg"
+crystal.TextColor3 = Color3.fromRGB(0, 0, 0)
+crystal.TextScaled = true
+crystal.TextSize = 14.000
+crystal.TextWrapped = true
+crystal.MouseButton1Down:Connect(function()
+	local A_1 = "Crystal Egg"
+	local Event = game:GetService("ReplicatedStorage").Events.OpenEgg
+	Event:FireServer(A_1)
+end)
+
+UICorner_32.Parent = crystal
 
 -- Scripts:
 
-local function YYQFPEK_fake_script() -- gui.LocalScript 
+local function RNBUOAC_fake_script() -- gui.LocalScript 
 	local script = Instance.new('LocalScript', gui)
 
 	local UIS = game:GetService("UserInputService")
@@ -396,4 +656,4 @@ local function YYQFPEK_fake_script() -- gui.LocalScript
 		end
 	end)
 end
-coroutine.wrap(YYQFPEK_fake_script)()
+coroutine.wrap(RNBUOAC_fake_script)()
