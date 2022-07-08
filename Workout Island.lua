493188b33731129587d2104d8a0dceca38324552
trotrosalaYT <86182280+trotrosalaYT@users.noreply.github.com>
2022-07-08 23:02:46 +0200

    Update Workout Island.lua

--- Workout Island.lua
+++ Workout Island.lua
@@ -507,3 +507,10 @@ local function GOVY_fake_script() -- gui.LocalScript
 	end)
 end
 coroutine.wrap(GOVY_fake_script)()
+
+local vu = game:GetService("VirtualUser")
+game:GetService("Players").LocalPlayer.Idled:connect(function()
+   vu:Button2Down(Vector2.new(0,0),workspace.CurrentCamera.CFrame)
+   wait(1)
+   vu:Button2Up(Vector2.new(0,0),workspace.CurrentCamera.CFrame)
+end)
