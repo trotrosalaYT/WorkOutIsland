49a7bcf38ac9bc105ad4fb906a252274b329ece1
trotrosalaYT <86182280+trotrosalaYT@users.noreply.github.com>
2022-07-31 17:36:04 +0200

    Add files via upload

---
+++ NekuLoaderNA.lua
@@ -0,0 +1,12 @@
+--Power Simulator 2
+if game.PlaceId == 4865990095 then
+	loadstring(game:HttpGet("https://raw.githubusercontent.com/trotrosalaYT/scripts/main/Power%20Simulator%202.lua", true))()
+end
+--Workout Island
+if game.PlaceId == 4032944086 then
+	loadstring(game:HttpGet("https://raw.githubusercontent.com/trotrosalaYT/scripts/main/Workout%20Island.lua", true))()		
+end
+--Mining Simulator 2
+if game.PlaceId == 9551640993 then
+	loadstring(game:HttpGet("https://raw.githubusercontent.com/trotrosalaYT/scripts/main/Mining%20Simulator%202.lua", true))()
+end
