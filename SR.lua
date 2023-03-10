6b803312b00256c58344390c9db2617b20a4132f
wedonttalkaboutbrun <86182280+wedonttalkaboutbrun@users.noreply.github.com>
2023-03-10 19:52:57 +0100

    Update SR.lua

--- SR.lua
+++ SR.lua
@@ -1,3 +1,4 @@
+_G.t = false
 local DiscordLib = loadstring(
     game:HttpGet "https://raw.githubusercontent.com/dawid-scripts/UI-Libs/main/discord%20lib.txt")()
 
@@ -71,7 +72,7 @@ end)
 
 buttons:Seperator()
 
-buttons:Slider("Speed", 0, 200, 16, function(t)
+buttons:Slider("Speed", 0, 1000, 16, function(t)
     game.Players.LocalPlayer.Character.Humanoid.WalkSpeed = t
 end)
 
@@ -84,10 +85,7 @@ misc:Bind("Visibility Toggle Bind", Enum.KeyCode.LeftAlt, function()
     end
 end)
 
-misc:Seperator()
-
-misc:Bind("Anti Afk Bind", Enum.KeyCode.RightShift, function()
-    _G.t = false
+misc2:Button("Anti Afk", function()
     if _G.t == false then
         DiscordLib:Notification("Successfully enabled AntiAfk",
             "You can now go afk for as long as you want, you wont disconnect from the afk kick", "Alright")
@@ -97,20 +95,10 @@ misc:Bind("Anti Afk Bind", Enum.KeyCode.RightShift, function()
             wait(1)
             ViUs:Button2Up(Vector2.new(0, 0), workspace.CurrentCamera.CFrame)
         end)
+        _G.t = true
     else
-        DiscordLib:Notification("Already enabled, no need to press again", "", "Alright")
+        DiscordLib:Notification("Already enabled", "", "Alright")
     end
-end)
-
-misc2:Button("Anti Afk", function()
-    DiscordLib:Notification("Successfully enabled AntiAfk",
-        "You can now go afk for as long as you want, you wont disconnect from the afk kick", "Alright")
-    local ViUs = game:GetService("VirtualUser")
-    game:GetService("Players").LocalPlayer.Idled:connect(function()
-        ViUs:Button2Down(Vector2.new(0, 0), workspace.CurrentCamera.CFrame)
-        wait(1)
-        ViUs:Button2Up(Vector2.new(0, 0), workspace.CurrentCamera.CFrame)
-    end)
 end)
 
 ew1:Toggle("Auto Open First Egg", false, function(bool)
