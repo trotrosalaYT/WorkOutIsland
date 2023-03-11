ae64577a649da7868ac8a96e6523a4856b4f8d97
wedonttalkaboutbrun <86182280+wedonttalkaboutbrun@users.noreply.github.com>
2023-03-11 15:19:29 +0100

    Update loader.lua

--- loader.lua
+++ loader.lua
@@ -3,3 +3,6 @@ if game.PlaceId == 12172698927 then
 else
     print("Not supported")
 end
+if game.PlaceId == 11040063484 then
+    loadstring(game:HttpGet"https://raw.githubusercontent.com/wedonttalkaboutbrun/scripts/main/Uwu.lua")()
+end
