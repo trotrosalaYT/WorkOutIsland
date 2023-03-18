debd29c53603ad6deb1178e4e5686fde5385501d
wedonttalkaboutbrun <86182280+wedonttalkaboutbrun@users.noreply.github.com>
2023-03-18 18:17:05 +0100

    Update loader.lua

--- loader.lua
+++ loader.lua
@@ -6,3 +6,6 @@ end
 if game.PlaceId == 11040063484 then
     loadstring(game:HttpGet"https://raw.githubusercontent.com/wedonttalkaboutbrun/scripts/main/Uwu.lua")()
 end
+if game.PlaceId == 11847010702 then
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/wedonttalkaboutbrun/scripts/main/GodlyLifters.lua"))()
+end
