f817a17573b8ffff0afe8641a940c93450541a04
wedonttalkaboutbrun <86182280+wedonttalkaboutbrun@users.noreply.github.com>
2023-03-20 15:11:01 +0100

    Update loader.lua

--- loader.lua
+++ loader.lua
@@ -1,11 +1,12 @@
 if game.PlaceId == 12172698927 then
     loadstring(game:HttpGet"https://raw.githubusercontent.com/wedonttalkaboutbrun/scripts/main/SR.lua")()
-else
-    print("Not supported")
 end
 if game.PlaceId == 11040063484 then
     loadstring(game:HttpGet"https://raw.githubusercontent.com/wedonttalkaboutbrun/scripts/main/Uwu.lua")()
 end
 if game.PlaceId == 11847010702 then
     loadstring(game:HttpGet("https://raw.githubusercontent.com/wedonttalkaboutbrun/scripts/main/GodlyLifters.lua"))()
+end
+if game.PlaceId == 12585099889 then
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/wedonttalkaboutbrun/scripts/main/SuperEvolution.lua"))()
 end
